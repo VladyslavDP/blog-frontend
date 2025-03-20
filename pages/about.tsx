8d668f2c45d941e17ec521da7eb84d2c1160aed6
Vladyslav Sharapat <>
2025-03-20 15:47:18 +0100

    S2103: Init project

---
+++ pages/about.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <div>
+            <h1>О проекте</h1>
+            <p>Этот проект демонстрирует использование Next.js с SSR и Nest.js в качестве API.</p>
+        </div>
+    );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+export async function getServerSideProps() {
+    const res = await fetch('http://localhost:4000/post/data');
+    const data = await res.json();
+
+    return { props: { posts: data } };
+}
+
+export default function Home({ data }) {
+    return (
+        <div>
+            <h1>data</h1>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+    );
+}
